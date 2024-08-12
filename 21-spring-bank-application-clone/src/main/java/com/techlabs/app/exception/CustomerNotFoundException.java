04f2461371342506c5c055b8bdebb086c3c0b8ba
nallashivamani <20eg106220@anurag.edu.in>
2024-08-12 14:23:38 +0530

    BankApplication

--- 21-spring-bank-application-clone/src/main/java/com/techlabs/app/exception/CustomerNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.techlabs.app.exception;
-
-public class CustomerNotFoundException extends RuntimeException{
-	private static final long serialVersionUID = 1L;
-
-	   public CustomerNotFoundException(String message) {
-	      super(message);
-	   }
-}
--- 21-spring-bank-application-clone/src/main/java/com/techlabs/app/repository/BankRepository.java
+++
@@ -1,9 +0,0 @@
-package com.techlabs.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.techlabs.app.entity.Bank;
-
-public interface BankRepository extends JpaRepository<Bank,Integer> {
-
-}
